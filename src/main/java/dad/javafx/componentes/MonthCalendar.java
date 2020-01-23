7b993b32da744555117cbdd8e4fd75efe53b758d
Pablogg98 <55440053+Pablogg98@users.noreply.github.com>
2020-01-23 09:54:13 +0000

    commit

--- src/main/java/dad/javafx/componentes/MonthCalendar.java
+++ src/main/java/dad/javafx/componentes/MonthCalendar.java
@@ -47,6 +47,7 @@ public class MonthCalendar extends GridPane implements Initializable {
 		month.addListener((o,ov,nv) -> onCambio(nv));
 		
 		if (LocalDate.now().getYear() == year.get() && LocalDate.now().getMonthValue() == month.get()) {
+			System.out.println("hola");
 			Label lb = new Label();
 			lb = (Label) getChildren().get(Calendar.getInstance().get(Calendar.DAY_OF_WEEK) + 7);
 			lb.getStyleClass().clear();
